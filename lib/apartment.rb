e9c507fd66c1d4da39a03b33709ec3975d1d8a93
mikekarnes123 <michaelkarnes.mk@gmail.com>
2019-02-06 09:29:56 -0700

    Adds Iteration 1

---
+++ lib/apartment.rb
@@ -0,0 +1,28 @@
+class Apartment
+  attr_reader :renter
+  def initialize(info)
+    @info = info
+    @renter = nil
+  end
+
+  def number
+    @info[:number]
+  end
+
+  def monthly_rent
+    @info[:monthly_rent]
+  end
+
+  def bathrooms
+    @info[:bathrooms]
+  end
+
+  def bedrooms
+    @info[:bedrooms]
+  end
+
+  def add_renter(renter)
+    @renter = renter 
+  end
+
+end
---
+++ test/apartment_test.rb
@@ -0,0 +1,32 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/apartment'
+require './lib/renter'
+
+class ApartmentTest < Minitest::Test
+  def setup
+    @a1 = Apartment.new({number: "A1",
+                        monthly_rent: 1200,
+                        bathrooms: 1,
+                         bedrooms: 1})
+    @jessie = Renter.new("Jessie")
+  end
+
+  def test_apartment_exists
+    assert_instance_of Apartment, @a1
+  end
+
+  def test_apartment_has_attributes
+    assert_equal "A1", @a1.number
+    assert_equal 1200, @a1.monthly_rent
+    assert_equal 1, @a1.bathrooms
+    assert_equal 1, @a1.bedrooms
+    assert_equal nil, @a1.renter
+  end
+
+  def test_add_renter
+    @a1.add_renter(@jessie)
+    assert_equal "Jessie", @a1.renter.name
+  end
+
+end
